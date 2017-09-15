9860fab9894b92c6d36e62c95847ad505cc386bf
muhammed <muhammed.thasneem@yahoo.com>
2017-09-15 16:44:23 +0530

    Initial commit

---
+++ GoogleMapWithRoad/app/src/main/java/pro/muhammed/com/googlemapwithroad/MainActivity.java
@@ -0,0 +1,13 @@
+package pro.muhammed.com.googlemapwithroad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
